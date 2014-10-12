8441612e38ae38ba959b33047eaa074c0a128aae
msempere <odoncopon@gmail.com>
2014-10-12 23:56:56 +0100

    Test path

--- src/markhor_cowboy.erl
+++ src/markhor_cowboy.erl
@@ -9,24 +9,25 @@ start() ->
     application:start(crypto),
     application:start(ranch),
     application:start(cowboy),
+    application:ensure_all_started(cowboy),
     Routes = routes(),
     Dispatch = cowboy_router:compile(Routes), 
     Port = port(),
-    io:fwrite("Markhor listening on port ~p~n",[Port]),
     TransOpts = [{port, Port},{max_connections, 2048}],
     ProtoOpts = [{env, [{dispatch, Dispatch}]},
                     {onrequest, fun request_hook_responder:set_cors/1},
                     {onresponse, fun error_hook_responder:respond/4}
-                ],    
+                ], 
     {ok, _}   = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
+    io:fwrite("Markhor listening on port ~p~n",[Port]),
     ok.
 
 routes() ->
     [
-        {"api.localhost",[
-                {"/v0.1/bid_request", bid_request_handler, []}
-             ]
-        }
+         {'_', [
+                {"/", bid_request_post_handler, []}
+               ]
+         }
     ]
 .
 
