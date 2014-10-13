50f92e0152b39b78aeca6708eaa4c8ecd2e6f1d4
msempere <odoncopon@gmail.com>
2014-10-13 19:40:19 +0100

    Agent generation

---
+++ src/agents.erl
@@ -0,0 +1,19 @@
+-module(agents).
+-author("msempere").
+-export([add_agent/2]).
+
+-record(creative,{width,
+                  height}).
+
+-record(agent,{name,
+               creatives=[]}).
+
+
+add_agent(agent = #agent{name = Name}, Dataset) -> 
+    lists:keystore(Name, #agent.name, Dataset, agent). 
+
+add_creative(agent = #agent{name = agentName}, Creative = #creative{}, Datase) -> 
+    lists:append(agent#agent.creatives, [Creative#creative{}]).
+
+
+
