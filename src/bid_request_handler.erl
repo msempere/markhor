bdcdeca3fc7e1fba34c5d2c9bffbdec7ea46345f
msempere <odoncopon@gmail.com>
2014-10-16 17:46:57 +0100

    New infrastructure for handlers

--- src/bid_request_handler.erl
+++ src/bid_request_handler.erl
@@ -1,25 +1,6 @@
 -module(bid_request_handler).
+-export([bid_handler/2]).
 
--export([init/3]).
--export([content_types_provided/2]).
--export([status/2]).
--export([terminate/3]).
-
-%% Init
-init(_Transport, _Req, []) ->
-    {upgrade, protocol, cowboy_rest}.
-
-
-%% Callbacks
-content_types_provided(Req, State) ->
-    {[      
-        {<<"application/json">>, status}    
-    ], Req, State}.
-
-terminate(_Reason, _Req, _State) ->
+bid_handler(Json, State) ->
+    Id = proplists:get_value(<<"id">>, Json); 
     ok.
-
-%% API
-status(Req, State) ->
-    Body = <<"{\"message\": \"bid_request_handler\", \"server\": \"markhor\", \"version\": \"0.1.0\"}">>,
-    {Body, Req, State}.
