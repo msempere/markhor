dc624868d62026b46c5bc6963411fbbaf3e29fb6
msempere <odoncopon@gmail.com>
2014-10-21 00:56:48 +0100

    Router adds new agent to the pool

--- src/markhor_router.erl
+++ src/markhor_router.erl
@@ -8,11 +8,20 @@ init() ->
 
 loop(Agents) ->
     receive
-        {From, Ref, bid_request} ->
-            io:fwrite("Router received bid request~n"),
+        {From, Ref, bid_request, Json} ->
+            From ! {Ref, ok_bid_request},
+            io:fwrite("Router received bid request:~n~p~n",[Json]),
             loop(Agents);
-        {From, Ref, Agent} ->
-            io:fwrite("Router received new agent~n"),
-            loop(Agents)
+
+        {From, Ref, agent, Agent} ->
+            From ! {Ref, ok_agent},
+            io:fwrite("Router received new agent:~n~p~n",[Agent]),
+            case markhor_objects:agent_in_list(Agent, Agents) of
+                true ->
+                    io:fwrite("Agent already exist~n"),
+                    loop(Agents);
+                false ->
+                    loop([Agent|Agents])
+            end
     end.
 
